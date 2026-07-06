00333298992df20d0d4c81fd3d6bd39618a3b472
Likith Raj H <likithrajh@Likiths-MacBook-Air.local>
2026-07-06 12:51:17 +0530

    third ede

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,8 +5,12 @@ import { ArrowRight, Zap } from 'lucide-react'
 import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
 import { Button } from './ui/button'
 import { PricingModal } from './PricingModal'
+import { checkUser } from '@/lib/checkUser'
+import { PLANS } from "@/lib/constants";
 
-const Header = () => {
+const Header = async () => {
+
+  const user = await checkUser();
   return (
   <header className="fixed w-full top-0 left-0 z-50 h-16 border-b border-white/6 bg-white/7 backdrop-blur-md">
     <nav className="mx-auto flex h-full max-w-7xl items-center justify-between px-4 sm:px-6 ">
@@ -20,12 +24,14 @@ const Header = () => {
                 Projects
             </Link>
 
-            <PricingModal>
-            <span className= "inline-flex h-8 items-center gap-1.5 rounded-full border border-white/10 bg-white/5 px-3 text-[13px] font-medium text-white/40 transition-colors hover:bg-white/10 hover:text-white/80">
-                <Zap className="h-3 w-3 fill-white/70" />
-                3/40 credits
-            
-            </span>
+            {user && (
+              <PricingModal>
+                <span className= "inline-flex h-8 items-center gap-1.5 rounded-full border border-white/10 bg-white/5 px-3 text-[13px] font-medium text-white/40 transition-colors hover:bg-white/10 hover:text-white/80">
+                  <Zap className="h-3 w-3 fill-white/70" />
+                  {user.credits} / {PLANS[user?.plan as Plan]} credits
+                </span>
+              </PricingModal>
+            )}
             
             <UserButton />
             </Show>
